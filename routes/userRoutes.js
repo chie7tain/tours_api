970400f163d23f21aae2d4f5a462474df2f14d46
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2025-07-16 12:16:17 +0100

    :art: Refresh db with more real world data
    refresh db with more real world data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,24 +3,27 @@ const router = require('express').Router();
 const userController = require('../controllers/userControllers');
 const authController = require('../controllers/authController');
 
-
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// auth
+router.use(authController.protect);
+// permission -> only admins
+router.use(authController.restrictTo('admin'));
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 router
   .route('/')
-  .get(authController.protect, userController.getAllUsers)
+  .get(userController.getAllUsers)
   .post(userController.createUser);
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-
 
 module.exports = router;
