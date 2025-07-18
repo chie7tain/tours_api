fa1830e7d6afd6cc3d55a0ac763829f68e273637
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2025-07-18 14:11:10 +0100

    :sparkles: Add base template using pug template
    add base template using pug template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const rateLimit = require('express-rate-limit');
 
@@ -19,7 +20,12 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 // 1 GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
 app.use(helmet());
 
@@ -59,14 +65,17 @@ app.use(
   }),
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'Testing',
+    user: 'Ikechukuw',
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
